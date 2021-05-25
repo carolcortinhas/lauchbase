91f0efcc706dc135f1e7e0086168122faf8dc616
carolcortinhas <carolinacortinhas@gmail.com>
2021-05-25 07:52:45 -0300

    Atualiza

---
+++ routes.js
@@ -0,0 +1,23 @@
+const express = require ('express')
+const routes = express.Router()
+const instrutores = require('./instrutores')
+
+routes.get('/', function (req, res){
+    return res.redirect("/instrutores")
+})
+
+routes.get('/instrutores', function (req, res){
+    return res.render("instrutores/index")
+})
+
+routes.get('/instrutores/create', function (req, res){
+    return res.render("instrutores/create")
+})
+
+routes.post("/instrutores", instrutores.post)
+
+routes.get('/membros', function (req, res){
+    return res.send("membros")
+})
+
+module.exports = routes
